2124cfb7264815e7e9f31707ee1b5067535db4e9
uriel-ewerton <uriel.ewerton1@gmail.com>
2025-07-30 17:23:15 -0300

    fix: Título da página

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { NavLink } from 'react-router-dom';
 import { HashLink } from 'react-router-hash-link';
 import { NavHashLink } from 'react-router-hash-link';
-import styles from './header.module.css';
+import styles from './Header.module.css';
 import { ThemeToggle } from '../ThemeToggle/ThemeToggle';
 
 const MenuIcon = () => <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}><path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" /></svg>;
